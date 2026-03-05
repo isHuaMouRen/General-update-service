b400d5f38504efce5de2dc1470c204093c689514
HuaMouRen <2825338723@qq.com>
2026-03-05 18:11:37 +0800

    Initialize

---
+++ GeneralUpdateService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneralUpdateService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
